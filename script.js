b1f015592f0e81e9e00de90596fced38e3d31024
Sergey Vorobyov <cerega.vorobjev@yandex.ru>
2022-10-30 17:51:20 +0300

    feat: Реализовать js и hover

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let editButton = document.querySelector(".profile__info-edit-button");
-console.log(editButton);
 //Функция открытия формы
 function editButtonOpen() {
     let editForm = document.querySelector(".edit-form");
@@ -7,11 +6,9 @@ function editButtonOpen() {
     editForm.classList.add("edit-form_opened");
     editForm.classList.remove("edit-form");
 } 
-
 editButton.addEventListener("click", editButtonOpen);
 
 let closeForm = document.querySelector(".edit-form__close-icon");
-console.log(closeForm);
 //Функция закрытия формы
 function closePopup() {
     let formEdit = document.querySelector(".edit-form_opened");
@@ -20,31 +17,20 @@ function closePopup() {
 }
 closeForm.addEventListener("click", closePopup);
 
-// Находим форму в DOM
-let formElement = document.querySelector(".popup-container");
-// Находим поля формы в DOM
+let formElement = document.querySelector(".edit-form");
 let nameInput = document.querySelector(".popup-container__name");
 let jobInput = document.querySelector(".popup-container__job");
 
-let save = document.querySelector(".popup-container__button");
-function saveClick() {
-    save.classList.add("popup-container__button_clicked");
-}
-save.addEventListener("click", saveClick);
-
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+//Функия отправки формы с ее закрытием
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     let profileInfoTitle = document.querySelector(".profile__info-title");
     let profileInfoSubtitle = document.querySelector(".profile__info-subtitle");
-    profileInfoTitle.textContent=`${nameInput.value}`;
-    profileInfoSubtitle.textContent=`${jobInput.value}`;
+    profileInfoTitle.textContent = nameInput.value;
+    profileInfoSubtitle.textContent = jobInput.value;
     console.log(profileInfoSubtitle);
     console.log(profileInfoTitle);
+    closePopup();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener("submit", formSubmitHandler);
