83c0ffb31af2a1e01c2670e7b2468e493f5a4816
Sergey Vorobyov <cerega.vorobjev@yandex.ru>
2022-11-29 15:25:23 +0300

    feat: Распределить валидацию в отдельный файл

--- scripts/script.js
+++ scripts/script.js
@@ -69,14 +69,6 @@ function addCard () {
   closePopup(popupCard);
 }
 
-function keyHandler(evt) {
-  //console.log(evt, '+');
-  evt.addEventListener("keydown", () => console.log(evt, 1));
-  if (evt.key ==='Escape') {
-    closePopup(evt);
-  } 
-}
-
 //Универсальная функция открытия попапа и добавления возможности закртыть окно при esc или клику вне формы
 function openPopup (popup) {
   popup.classList.add("popup_opened");
@@ -100,17 +92,6 @@ function openPopupImage(item, itemImg) {
 
 nameInput.value = profileInfoTitle.textContent;
 jobInput.value = profileInfoSubtitle.textContent;
-
-//Функция, которая делает кнопку добавления новой карточки всегда неактивной при открытии попапа
-function setSubmitButtonState(isFormValid) {
-  if (isFormValid) {
-    buttonSubmitCard.removeAttribute('disabled');
-    buttonSubmitCard.classList.remove('popup__button_disabled');
-  } else {
-    buttonSubmitCard.setAttribute('disabled', true);
-    buttonSubmitCard.classList.add('popup__button_disabled');
-  }
-}
 
 //Функция отправки формы профиля с ее закрытием
 function handleFormProfileSubmit (evt) {
@@ -136,77 +117,6 @@ function openBigImg (item, itemImg) {
   popupImageBackground.alt = item;
 }
 
-//Добавление стиля для неверно введенного инпута
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add("popup__input_type_error");
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add("popup__input-error_active");
-};
-
-//Удаления стиля для неверно введенного инпута
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove("popup__input_type_error");
-  errorElement.classList.remove("popup__input_error_active");
-  errorElement.textContent = '';
-}
-
-//Проверка инпута на валидность и применение стилей к соотвествующему значению
-const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-}
-
-//Установка слушателя на каждый инпут
-const setEventListener = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
-  const buttonElement = formElement.querySelector(".popup__button");
-  toggleButtonState(inputList, buttonElement);
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener("input", () => {
-      toggleButtonState(inputList, buttonElement);
-      checkInputValidity(formElement, inputElement);
-    });  
-  });
-}
-
-//Добавление каждого попапа в цикл для дальнейшей проверки внутри него каждого инпута на валидность
-const enableValidation = () => {
-  const popupList = Array.from(document.querySelectorAll(".popup"));
-  popupList.forEach((formElement) => {
-    formElement.addEventListener("submit", (evt) => {
-      evt.preventDefault();
-    });
-    const containerList = Array.from(formElement.querySelectorAll(".popup__container"));
-    containerList.forEach((item) => {
-      setEventListener(item);
-    });
-  });
-}
-
-//Функия, которая возвращает true или false в зависимости от валидности инпута
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-}
-
-//Функция, при которой кнопка сабмита неактивна, если поля неверно заполены
-const toggleButtonState = (inputList, buttonElement) => {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add("popup__button_disabled");
-    buttonElement.setAttribute('disabled', true)
-  } else {
-    buttonElement.classList.remove("popup__button_disabled");
-    buttonElement.removeAttribute('disabled', false);
-  }
-}
-
-enableValidation();
 editButton.addEventListener("click", () => openPopup(popupProfile));
 buttonAddCard.addEventListener("click", () => openPopup(popupCard));
 formElementProfile.addEventListener("submit", handleFormProfileSubmit);
---
+++ scripts/validate.js
@@ -0,0 +1,92 @@
+//Функция, которая делает кнопку добавления новой карточки всегда неактивной при открытии попапа
+function setSubmitButtonState(isFormValid) {
+    if (isFormValid) {
+      buttonSubmitCard.removeAttribute('disabled');
+      buttonSubmitCard.classList.remove('popup__button_disabled');
+    } else {
+      buttonSubmitCard.setAttribute('disabled', true);
+      buttonSubmitCard.classList.add('popup__button_disabled');
+    }
+  }  
+
+//Добавление стиля для неверно введенного инпута
+const showInputError = (formElement, inputElement, errorMessage) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add("popup__input_type_error");
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add("popup__input-error_active");
+  };
+  
+  //Удаления стиля для неверно введенного инпута
+  const hideInputError = (formElement, inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove("popup__input_type_error");
+    errorElement.classList.remove("popup__input_error_active");
+    errorElement.textContent = '';
+  }
+  
+  //Проверка инпута на валидность и применение стилей к соотвествующему значению
+  const checkInputValidity = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+      showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+      hideInputError(formElement, inputElement);
+    }
+  }
+  
+  //Установка слушателя на каждый инпут
+  const setEventListener = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
+    const buttonElement = formElement.querySelector(".popup__button");
+    toggleButtonState(inputList, buttonElement);
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        toggleButtonState(inputList, buttonElement);
+        checkInputValidity(formElement, inputElement);
+      });  
+    });
+  }
+  
+  //Добавление каждого попапа в цикл для дальнейшей проверки внутри него каждого инпута на валидность
+  const enableValidation = () => {
+    const popupList = Array.from(document.querySelectorAll(".popup"));
+    popupList.forEach((formElement) => {
+      formElement.addEventListener("submit", (evt) => {
+        evt.preventDefault();
+      });
+      const containerList = Array.from(formElement.querySelectorAll(".popup__container"));
+      containerList.forEach((item) => {
+        setEventListener(item);
+      });
+    });
+  }
+  
+  //Функия, которая возвращает true или false в зависимости от валидности инпута
+  const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
+  
+  //Функция, при которой кнопка сабмита неактивна, если поля неверно заполены
+  const toggleButtonState = (inputList, buttonElement) => {
+    if (hasInvalidInput(inputList)) {
+      buttonElement.classList.add("popup__button_disabled");
+      buttonElement.setAttribute('disabled', true)
+    } else {
+      buttonElement.classList.remove("popup__button_disabled");
+      buttonElement.removeAttribute('disabled', false);
+    }
+  }
+
+// включение валидации вызовом enableValidation
+// все настройки передаются при вызове
+
+enableValidation({
+    formElement: '.popup',
+    inputElement: '.popup__input',
+    buttonSubmitCard: '.popup__button',
+    buttonSubmitCard: 'popup__button_disabled',
+    inputElement: 'popup__input_type_error',
+    errorElement: 'popup__error_visible'
+  });
